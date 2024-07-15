4794205c10d604c5a10a50a2fbe5f268caaa4977
ews578 <ewsnider28@yahoo.com>
2024-07-15 09:54:14 -0500

    added some sections to the home page very vanilla but will style later after logic is finished

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,19 +1,75 @@
-export default function Home() {
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image"; // Import the Image component
+
+function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <>
+
+      <ContainerIntro />
+      <ServicesProvided />
+      <ServiceArea />
+      <Choice />
+    </>
+  );
+}
+
+function ContainerIntro() {
+  return (
+    <div className="intro">
+      <Row>
+        <Col>
+      <Card>
+        <Card.Img variant="top" src="assets/kansascity.jpg" />
+      </Card>
+          <h1>Kansas City's Trusted Heating and Cooling Company</h1>
+          <p>
+            KC Air Repair is dedicated to delivering top-notch service and
+            ensuring customer satisfaction in every aspect of our business.
+            Reach out today and discover how KC Air can help you achieve your
+            goals.
+          </p>
+        </Col>
+      </Row>
     </div>
   );
 }
+
+
+function ServicesProvided() {
+  return (
+    <div className="services-provided">
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image src="assets/coolfam.jpg" rounded /> {/* Use the Image component with the correct src */}
+            <h1>Services We Offer</h1>
+            <p>A list of services placed here</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+function ServiceArea() {
+  return (
+    <div className="service-area">
+      <h1>Areas we service</h1>
+      <p>A table of cities of where we service will be placed here</p>
+    </div>
+  );
+}
+
+function Choice() {
+  return (
+    <div className="choice">
+      <h1>Why choose us?</h1>
+      <p>A paragraph explaining why we are the best option for heating and cooling services in the Kansas City area</p>
+    </div>
+  );
+}
+
+export default Home;
